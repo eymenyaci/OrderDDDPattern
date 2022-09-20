0233db74030d2c1a46d9e3481bb0bb37a49f049d
eymenyaci <eymenyaci@gmail.com>
2022-09-20 23:46:17 +0300

    complated base domain driven design pattern

---
+++ Order.Application/DomainEventHandler/OrderStartedDomainEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Order.Application.Repository;
+using Order.Domain.AggregateModels.BuyerModels;
+using Order.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Order.Application.DomainEventHandler
+{
+    public class OrderStartedDomainEventHandler : INotificationHandler<OrderStartedDomainEvent>
+    {
+        private readonly IBuyerRepository buyerRepository;
+
+        public OrderStartedDomainEventHandler(IBuyerRepository buyerRepository)
+        {
+            this.buyerRepository = buyerRepository;
+        }
+        public Task Handle(OrderStartedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            if (notification.Order.UserName == "")
+            {
+                var buyer = new Buyer(notification.UserName);
+                // buyerRepository.Add(buyer); // Create buyer and get id
+
+                // set order's buyerId
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
